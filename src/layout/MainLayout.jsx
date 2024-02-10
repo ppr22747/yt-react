4990a4ffccb17faeb2dd0a36c112e925e0138843
Weerapat <weerapat.somtua@gmail.com>
2024-02-10 23:59:08 +0700

    Initial commit

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,15 @@
+import Nav from "../components/Nav/Nav";
+import Sidebar from "../components/Sidebar/Sidebar";
+import Content from "../components/Content/Content";
+
 function MainLayout() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section id="main-layout">
+      <Nav></Nav>
+      <Sidebar></Sidebar>
+      <Content></Content>
+    </section>
+  );
 }
 
 export default MainLayout
